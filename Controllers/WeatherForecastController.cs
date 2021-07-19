9aa2974d67475f405e9f8c0c7d2125eb4150bd0d
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-07-18 22:41:07 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SerilogDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation($"{nameof(WeatherForecastController)} intanciando ILogger");
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation($"{nameof(Get)} Endpoint");
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    if (i == 3)
+                        throw new Exception("Ocorreu uma excessão");
+
+                    _logger.LogInformation($"Número de iterações: {i}");
+                }
+
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"{nameof(Get)} Endpoint - Ocorreu uma excessão");
+                throw;
+            }
+
+        }
+    }
+}
